4e450aea42af059c11e11f2f59f5fd8cf43589be
Victor Matheus <victormatheus183@gmail.com>
2022-12-21 22:12:23 -0300

    player manager created
    by now defines if its will move passing a bool value to movement script;
    
    Detects if hits an obstacle ou a coin and execute the respective logic

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+using Scripts.Core.Interfaces;
+
+namespace Assets.Scripts.Player
+{
+    [RequireComponent(typeof(PlayerMovement))]
+    public class Player : MonoBehaviour
+    {
+        private string _obstacleTag = "Obstacle";
+        private PlayerMovement _playerMovement;
+
+        // Use this for initialization
+        void Start()
+        {
+            InitComponents();
+            InitPlayerStatusToMovement();
+        }
+        private void InitComponents()
+        {
+            _playerMovement = GetComponent<PlayerMovement>();
+        }
+        private void InitPlayerStatusToMovement()
+        {
+            _playerMovement.isAlive = true;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter(Collider collision)
+        {
+            IScorable scorableGameObject = collision.gameObject.GetComponent<IScorable>();
+            if (scorableGameObject != null)
+            {
+                scorableGameObject.IncreaseScore();
+            }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag(_obstacleTag))
+            {
+                _playerMovement.isAlive = false;
+            }
+        }
+
+    }
+}
