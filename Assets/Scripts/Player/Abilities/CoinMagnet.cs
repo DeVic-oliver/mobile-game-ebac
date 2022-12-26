7dc760b9350be27eb83d3640aa5f175badac2de6
Victor Matheus <victormatheus183@gmail.com>
2022-12-26 20:03:06 -0300

    Player coin magnet ability set

---
+++ Assets/Scripts/Player/Abilities/CoinMagnet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using CoinMagnetPowerUp = Assets.Scripts.Items.Powerups.Abilities.CoinMagnet;
+using CoinMagnetModule = Assets.Scripts.Player.Abilities.Modules.CoinMagnet;
+
+namespace Assets.Scripts.Player.Abilities
+{
+    public class CoinMagnet : MonoBehaviour
+    {
+        [SerializeField] private CoinMagnetModule coinMagnetGameObject;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            CoinMagnetPowerUp powerUp = other.gameObject.GetComponent<CoinMagnetPowerUp>();
+            if (powerUp != null)
+            {
+                powerUp.gameObject.SetActive(false);
+                coinMagnetGameObject.ActiveAbilityBuff();
+            }
+        }
+    }
+}
