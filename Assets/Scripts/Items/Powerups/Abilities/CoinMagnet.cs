ff28dbb827bcf2f54c134c0d401e5b870e8f3a60
Victor Matheus <victormatheus183@gmail.com>
2022-12-26 20:02:30 -0300

    coin magnet powerup created

---
+++ Assets/Scripts/Items/Powerups/Abilities/CoinMagnet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Items.Powerups.Abilities
+{
+    public class CoinMagnet : MonoBehaviour
+    {
+
+        private void Start()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void EnableAbility()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
