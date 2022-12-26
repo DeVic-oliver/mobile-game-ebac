ddec9689ebf1bfc1a42c7e05661eb3318d1d6bda
Victor Matheus <victormatheus183@gmail.com>
2022-12-26 14:24:41 -0300

    Buff methods overload

--- Assets/Scripts/Core/Interfaces/IBuffable.cs
+++ Assets/Scripts/Core/Interfaces/IBuffable.cs
@@ -3,6 +3,7 @@ namespace Scripts.Core.Interfaces
 {
     public interface IBuffable 
     {
-        public float ApplyBuff();
+        public float GetBuffedValue(float valueToBeBuffed);
+        public int GetBuffedValue(int valueToBeBuffed);
     }
 }
