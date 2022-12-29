86ef16ec04495d3ba0afbbf42ce987f6159666aa
Victor Matheus <victormatheus183@gmail.com>
2022-12-29 14:33:55 -0300

    a animator component

---
+++ Assets/Scripts/Core/Manager/AnimationManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Core.Manager
+{
+    public class AnimationManager : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+
+        public void TriggerAnimation(string animationID)
+        {
+            _animator.SetTrigger(animationID);
+        }
+    }
+}
